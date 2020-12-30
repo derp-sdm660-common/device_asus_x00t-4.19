534a5c07f744179833afa468a19bf9229b36f65a
rahul9999xda <rahul9999xda@gmail.com>
2020-12-30 19:38:06 +0530

    sdm660-common: Update Remove packages list
    Signed-off-by: rahul9999xda <rahul9999xda@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt arcore DevicePolicyPrebuilt Drive MyVerizonServices Ornament OBDM_Permissions OemDmTrigger SafetyHubPrebuilt ScribePrebuilt Showcase SprintDM SprintHM SoundAmplifierPrebuilt YouTube YouTubeMusicPrebuilt VZWAPNLib VzwOmaTrigger WallpapersBReel2020 obdm_stub
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt arcore DevicePolicyPrebuilt Drive GoogleCamera MyVerizonServices Ornament OBDM_Permissions OemDmTrigger PixelWallpapers2020 PixelLiveWallpaperPrebuilt SafetyHubPrebuilt ScribePrebuilt Showcase SprintDM SprintHM SoundAmplifierPrebuilt YouTube YouTubeMusicPrebuilt VZWAPNLib VzwOmaTrigger WallpapersBReel2020 obdm_stub
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
