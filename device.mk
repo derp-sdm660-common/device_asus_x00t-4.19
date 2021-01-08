825d498dd2e8b3e5b01317545dd8d40948d7ec45
Pranav Kapoor <pranavkapoor001@gmail.com>
2021-01-08 11:09:46 +0530

    X00T: Bringup ArrowOS
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- device.mk
+++ device.mk
@@ -220,13 +220,13 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay #\
+#    $(LOCAL_PATH)/overlay-lineage
 
 # Overlays - Enforce RRO
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+#    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Perf (psiclient dep)
 PRODUCT_PACKAGES += \
