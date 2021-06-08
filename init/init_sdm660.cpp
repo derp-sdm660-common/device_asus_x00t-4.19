2680bc06c63b88c4642d22767296f4dbbb095a48
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-06-08 13:46:57 +0000

    X00T: Disable oem unlock
    * Fixes safetynet
    
    Signed-off-by: rahul9999xda <rahul9999xda@gmail.com>

--- init/init_sdm660.cpp
+++ init/init_sdm660.cpp
@@ -113,6 +113,7 @@ void vendor_load_properties()
 
    // Safetynet Workaround
     property_override("ro.boot.verifiedbootstate", "green");
+    property_override("ro.oem_unlock_supported", "0");
     property_override("ro.build.description", "walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys");
     property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
     property_override("ro.system.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
